3d8c4c591c10831dd94f8afb3678d332235c64b4
german <gkalitve@gmail.com>
2023-11-25 22:00:13 +0000

    Introduce DOM methods; simplify conditions with switch statements and ternary operators

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,61 +1,89 @@
-game();
+const body = document.querySelector("body");
+let playerScore = 0;
+let cpuScore = 0;
+
+// add text with instructions to press one of the buttons (1st row)
+const instructions = document.createElement("div");
+instructions.textContent = "Please select 'Rock', 'Paper' or 'Scissors' to play.";
+body.appendChild(instructions);
+
+// add string that announces the outcome of each round (2nd row)
+let playRoundResult = document.createElement("div");
+body.appendChild(playRoundResult);
+
+// add string that keeps track of the score (3rd row)
+let scoreBoard = document.createElement("div");
+scoreBoard.textContent = `Player: ${playerScore} CPU: ${cpuScore}`;
+body.appendChild(scoreBoard);
+
+// add string that announces the winner of a game (4th row)
+let gameResult = document.createElement("div");
+body.appendChild(gameResult);
+
+// if the player clicks a button at the end of a game (when the gameResult has been announced), reset playerScore, cpuScore, and gameResult to "0"
+let btn = document.querySelectorAll("button");
+btn.forEach(function(i) {
+    i.addEventListener('click', function( event ) {
+    if (playerScore == 5 || cpuScore == 5) {
+        playerScore = 0;
+        cpuScore = 0; 
+        playRoundResult.textContent = "";
+        scoreBoard.textContent = `Player: ${playerScore} CPU: ${cpuScore}`;
+        gameResult.textContent = "";
+        event.stopImmediatePropagation();
+        }
+    })   
+})
+
+
+let rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+playRound("Rock", getComputerChoice());
+});
+
+let paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+playRound("Paper", getComputerChoice());
+});
+
+let scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+playRound("Scissors", getComputerChoice());
+});
 
 // run function that generates computer's input getComputerChoice
 function getComputerChoice() {
     let computerSelection = (Math.random());
-    if (computerSelection < (1/3)) {
-        computerSelection = "Rock";
-    } else if (computerSelection >= (1/3) && computerSelection < (2/3)) {
-        computerSelection = "Paper";
-    } else {
-        computerSelection = "Scissors";
-    } 
+    computerSelection = (computerSelection < (1/3)) ? "Rock"
+                      : (computerSelection >= (1/3) && computerSelection < (2/3)) ? "Paper"
+                      : "Scissors";
     return computerSelection;
 }
 
-// run a function that continues until Player or CPU reaches 5 victories
-function game() {
-
-let playerScore = 0;
-let cpuScore = 0;
-while ((playerScore < 5) && (cpuScore < 5)) {
-
-// prompt() user to input rock, paper or scissors; capitalize the input
-let playerSelection = prompt("Input either 'Rock', 'Paper', or 'Scissors'");
-playerSelection = (playerSelection.substring(0, 1).toUpperCase() + playerSelection.substring(1).toLowerCase());
-
-// converting output of playRound into a single string and evaluating whether it's a "Win" or a "Lose" to keep score
-let playRoundOutcome = (playRound(playerSelection, getComputerChoice()));
-if (playRoundOutcome.includes("Win")) {
-       playerScore++;
-   } else if (playRoundOutcome.includes("Lose")) {
-       cpuScore++;    
-   }
-console.log(playRoundOutcome);
-console.log(`Player: ${playerScore} - CPU: ${cpuScore}`);
-
-//run function that plays 1 round of RPS taking playerSelection and computerSelection
-function playRound(playerSelection, computerSelection) {
-    if ((playerSelection === "Rock") || (playerSelection === "Paper") || (playerSelection === "Scissors")) {
+  //run function that plays 1 round of RPS taking playerSelection and computerSelection
+  function playRound(playerSelection, computerSelection) {
         // comparing playerSelection with computerSelection
-        if (playerSelection === computerSelection) {
-        x = `It's a tie! Player and CPU both picked ${playerSelection}.`;
-    } else if 
-         (((playerSelection === "Rock") && (computerSelection === "Scissors")) || 
-         ((playerSelection === "Paper") && (computerSelection === "Rock")) ||
-         ((playerSelection === "Scissors") && (computerSelection === "Paper"))) {
-         x = `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-         x = `You Lose. CPU picked ${computerSelection}. ${computerSelection} beats ${playerSelection}.`;
-    }
-    }  else { x = "You did not enter 'Rock', 'Paper', or 'Scissors'. Please try again!";
-    }
-    return x;
-    }
+        switch (true) {
+            case (playerSelection === computerSelection):
+                playRoundResult.textContent = `It's a tie! Player and CPU both picked ${playerSelection}.`;
+                break;
+            case (((playerSelection === "Rock") && (computerSelection === "Scissors")) || 
+                  ((playerSelection === "Paper") && (computerSelection === "Rock")) ||
+                  ((playerSelection === "Scissors") && (computerSelection === "Paper"))):
+                playRoundResult.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+                playerScore++;
+                break;
+            default:
+                playRoundResult.textContent = `You Lose. CPU picked ${computerSelection}. ${computerSelection} beats ${playerSelection}.`;
+                cpuScore++;
+        }
+
+        // updating the scoreBoard
+        scoreBoard.textContent = `Player: ${playerScore} CPU: ${cpuScore}`;
+
+        // checking whether to post the gameResult announcement and determining which one
+        gameResult.textContent = (playerScore == 5) ? ("You won the game. Congrats!")
+                               : (cpuScore == 5) ? ("CPU won the game. You lose!") 
+                               : "";
   }
-  // announcing conclusion of the game
-  let z = (playerScore > cpuScore) ?  "Congrats! You Won the Game!" : "Too Bad! CPU Won the Game!" ;
-  console.log(z);
-}
-
-
+ 
